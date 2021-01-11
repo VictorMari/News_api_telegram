8d38e234bef174291a18bfb924c77867d8f7a5ad
VictorMari <vmarirodriguez@gmail.com>
2021-01-11 01:51:44 +0000

    sending news with formated text to hardcoded chat

--- .aws-sam/build/HelloWorldFunction/app.js
+++ .aws-sam/build/HelloWorldFunction/app.js
@@ -22,6 +22,32 @@ const articles = {
             "urlToImage": "https://ichef.bbci.co.uk/news/1024/branded_news/0546/production/_116405310_hi065113789.jpg",
             "publishedAt": "2021-01-09T08:14:51Z",
             "content": "Storm Filomena has blanketed parts of Spain in heavy snow, with half of the country on red alert for more on Saturday. \r\nMadrid, one of the worst affected areas, is set to see up to 20cm (eight inche… [+1189 chars]"
+        },
+        {
+            "source": {
+                "id": null,
+                "name": "New York Times"
+            },
+            "author": "Katherine Rosman",
+            "title": "Hilaria Baldwin Talks Spain, Boston, Alec and Instagram",
+            "description": "Accused of a “decade long grift where she impersonates a Spanish person,” the entrepreneur (and spouse of Alec Baldwin) talks about being the main character in this last week of 2020.",
+            "url": "https://www.nytimes.com/2020/12/30/style/hilaria-baldwin-interview.html",
+            "urlToImage": "https://static01.nyt.com/images/2020/12/29/fashion/29HILARIA/29HILARIA-facebookJumbo.jpg",
+            "publishedAt": "2020-12-30T10:00:40Z",
+            "content": "Ms. Baldwin first visited Spain with her parents when she was a baby, she said, and she went at least yearly thereafter. She declined to explain in detail how frequently they traveled there or how lo… [+1529 chars]"
+        },
+        {
+            "source": {
+                "id": "bbc-news",
+                "name": "BBC News"
+            },
+            "author": "https://www.facebook.com/bbcnews",
+            "title": "Spain housing: Evictions and broken promises in a pandemic",
+            "description": "Spain's prime minister promised nobody would be evicted during the pandemic but activists say thousands have.",
+            "url": "https://www.bbc.co.uk/news/55231201",
+            "urlToImage": "https://ichef.bbci.co.uk/news/1024/branded_news/15AB6/production/_115985788_photo1.jpg",
+            "publishedAt": "2020-12-14T00:12:00Z",
+            "content": "By Tim SmithBarcelona\r\nimage captionCecilia's family have been sent an eviction notice and fear what may happen next\r\nEvictions are an unwelcome fact of life in Ciutat Meridiana, one of Barcelona's p… [+5878 chars]"
         }]
 }
 
@@ -52,7 +78,7 @@ exports.lambdaHandler = async (event, context) => {
         }
         response = {
             'statusCode': 200,
-            'body': "asdjksdafjkfsdajksafd"
+            'body': "asdjksdafjkfsdajksafd\n"
         }
     } catch (err) {
         console.log(err);
@@ -82,7 +108,8 @@ function formatNews(news){
     let message = ""
     
     news.forEach(article => {
-        message += `${article.title}`
+        message += `<strong>Title: </strong>${article.title}\n`
+        message += `<a href="${article.url}">Article link</a>\n`
     })
     return message
 }
@@ -92,7 +119,8 @@ async function sendChatMessage(chatId, message){
     let telegramUrl=`https://api.telegram.org/${token}/sendMessage`
     let jsonBody = {
         chat_id: chatId,
-        text: message
+        text: message,
+        parse_mode: "HTML"
     }
 
     let req = {
